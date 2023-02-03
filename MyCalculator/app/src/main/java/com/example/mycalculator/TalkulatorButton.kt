68a2793d5733b6387253e2e6aca8f107b1268f96
sannio82 <sannajojo82@gmail.com>
2023-02-03 13:36:14 +0100

    Error ButtonSymbol fixed, text added

--- MyCalculator/app/src/main/java/com/example/mycalculator/TalkulatorButton.kt
+++ MyCalculator/app/src/main/java/com/example/mycalculator/TalkulatorButton.kt
@@ -1,45 +1,52 @@
-package com.example.mycalculator
+    package com.example.mycalculator
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+    import androidx.compose.foundation.clickable
+    import androidx.compose.foundation.layout.Box
+    import androidx.compose.foundation.shape.CircleShape
+    import androidx.compose.material.Text
+    import androidx.compose.runtime.Composable
+    import androidx.compose.ui.Alignment
+    import androidx.compose.ui.Modifier
+    import androidx.compose.ui.draw.clip
+    import androidx.compose.ui.graphics.Color
+    import androidx.compose.ui.unit.sp
 
-@Composable
-fun ButtonNumber(
-    number: String,
-    onClick: () -> Unit,
-) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .clip(CircleShape)
-            .clickable { onClick() }
-    )
-    {
-        Text(
-            text = number,
-            fontSize = 36.sp,
-            color = Color.Red
+    @Composable
+    fun ButtonNumber(
+        number: String,
+        onClick: () -> Unit,
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .clip(CircleShape)
+                .clickable { onClick() }
         )
+        {
+            Text(
+                text = number,
+                fontSize = 36.sp,
+                color = Color.Red
+            )
+        }
     }
-}
 
-@Composable
-fun ButtonSymbol(
-    symbol: String,
-    onClick: () -> Unit,
-) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .clip(CircleShape)
-            .clickable { onClick() }
-    )
-}
+    @Composable
+    fun ButtonSymbol(
+        symbol: String,
+        onClick: () -> Unit,
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .clip(CircleShape)
+                .clickable { onClick() }
+        )
+        {
+            Text(
+                text = symbol,
+                fontSize = 36.sp,
+                color = Color.Red
+            )
+        }
+    }
